f63aca7cc18ace500ba66c352bb16e02148ba2fd
Boris <zhouboli@gmail.com>
2015-10-29 17:57:07 +0800

    Initial Commit

---
+++ Oversea/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Oversea
+//
+//  Created by Zhouboli on 15/10/29.
+//  Copyright © 2015年 Bankwel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
